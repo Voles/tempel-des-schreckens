6c3382fff756ae2a1038f3d5380a27b2efe326dd
Thomas Coopman <thomas.coopman@gmail.com>
2018-11-16 20:42:50 +0100

    add start controller

--- tempel_des_schreckens/lib/schreckens_web/controllers/game_controller.ex
+++ tempel_des_schreckens/lib/schreckens_web/controllers/game_controller.ex
@@ -1,7 +1,13 @@
 defmodule SchreckensWeb.GameController do
   use SchreckensWeb, :controller
 
+  def start(conn, %{"playerCount" => playerCount}) when playerCount >= 3 and playerCount <= 10 do
+    json(conn, "ok")
+  end
+
   def start(conn, _params) do
-    json(conn, "")
+    conn
+    |> put_status(400)
+    |> json("error")
   end
 end
--- tempel_des_schreckens/test/schreckens_web/controllers/game_controller_test.exs
+++ tempel_des_schreckens/test/schreckens_web/controllers/game_controller_test.exs
@@ -1,8 +1,13 @@
 defmodule SchreckensWeb.GameControllerTest do
   use SchreckensWeb.ConnCase
 
-  test "POST /start", %{conn: conn} do
-    conn = post(conn, "/start")
-    assert json_response(conn, 200)
+  test "POST /start with correct arguments", %{conn: conn} do
+    conn = post(conn, "/start", %{playerCount: 3})
+    assert "ok" = json_response(conn, 200)
+  end
+
+  test "POST /start with incorrect arguments", %{conn: conn} do
+    conn = post(conn, "/start", %{playerCount: 11})
+    assert "error" = json_response(conn, 400)
   end
 end
